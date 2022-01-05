e2ebf09ad69e0b0b7ae2e362b2a75d2fdacbf3a6
Jakob Arvidsson <j.arvidsson.97@gmail.com>
2022-01-05 20:34:37 +0100

    add winner page. add cassette player. edit multiple puzzles

--- scripts/main.js
+++ scripts/main.js
@@ -45,118 +45,161 @@
       console.error('Error during service worker registration:', e);
     });
   }
+})();
 
-  var audio_correct = document.getElementById("a_correct")
-  var audio_wrong = document.getElementById("a_wrong")
-  var delay = 1500;
-  document.addEventListener('keyup', function(e){
-    if(e.key === 'Enter'){
-      var page = getPage();
-      checkpwd(page);
-    }
-  });
+const audio_correct = document.getElementById("a_correct")
+let audio_wrong = document.getElementById("a_wrong")
+const delay = 1500;
+
+document.addEventListener('keyup', function(e){
+  if(e.key === 'Enter'){
+    var page = getPage();
+    checkpwd(page);
+  }
+});
 
 
-  function getPage(){
-    var href = window.location.href;
-    var page_t = href.slice(-7, -6);
-    var page_e = href.slice(-6, -5);
+function getPage(){
+  var href = window.location.href;
+  var page_t = href.slice(-7, -6);
+  var page_e = href.slice(-6, -5);
 
-    if(page_t == "e"){
-      return page_e;
-    }
-    else{
-      return page_t + page_e;
-    }
-
+  if(page_t == "e"){
+    return page_e;
+  }
+  else{
+    return page_t + page_e;
   }
 
-  function getPagePw(p){
-    var pw;
-    switch(p){
-      case "1":
-        //Informer
-        pw = "49";
-        break;
+}
 
-      case "2":
-        //Sagrada familia
-        pw = "172,50";
-        break;
-      
-      case "3":
-        //Periodic table
-        pw = "89285531";
-        break;
-
-      case "4":
-        //Count numbers
-        pw = "13112221";
-        break;
-
-      case "5":
-        //Reciept
-        pw = "ratatouille";
-        break;
-
-      case "6":
-        //Sunshine
-        pw = "war";
-        break;
-
-      case "7":
-        //Binary coords
-        pw = "stonehenge";
-        break;
-
-      case "8":
-        //Mumin
-        pw = "little my";
-        break;
-
-      case "9":
-        //Davinci
-        pw = "";
-        break;
-
-      case "10":
-        //Bot
-        pw = "discover";
-        break;
-
-      case "11":
-        //Ceasar
-        pw = "discover";
-        break;
-
-      case "12":
-      //Start text
-      pw = "think";
+function getPagePw(p){
+  var pw;
+  switch(p){
+    case "1":
+      //Informer
+      pw = "49";
       break;
 
-    }
-    return pw;
-  }
-  
-
-  function checkpwd(page){
-    var pagePassword = getPagePw(page);
-    var password = document.getElementById("psw").value;
-    var pNext = parseInt(page) + 1;
+    case "2":
+      //Sagrada familia
+      pw = "172,50";
+      break;
     
-    if(password == pagePassword){
-      audio_correct.play();
-      setTimeout(function(){
-        window.location.href = "page" + pNext + ".html";
-      }, delay);
+    case "3":
+      //Periodic table
+      pw = "pulp fiction";
+      break;
+
+    case "4":
+      //Count numbers
+      pw = "13112221";
+      break;
+
+    case "5":
+      //Reciept
+      pw = "ratatouille";
+      break;
+
+    case "6":
+      //Sunshine
+      pw = "war";
+      break;
+
+    case "7":
+      //Binary coords
+      pw = "stonehenge";
+      break;
+
+    case "8":
+      //Mumin
+      pw = "little my";
+      break;
+
+    case "9":
+      //Davinci
+      pw = "shoemakers";
+      break;
+
+    case "10":
+      //Bot
+      pw = "hedgehog";
+      break;
+
+    case "11":
+      //Ceasar
+      pw = "discover";
+      break;
+
+    case "12":
+    //Start text
+    pw = "think";
+    break;
+
+  }
+  return pw;
+}
+
+
+function checkpwd(page){
+  var pagePassword = getPagePw(page);
+  var password = document.getElementById("psw").value;
+  var pNext = parseInt(page) + 1;
+  
+  if(password == pagePassword){
+    audio_correct.play();
+    setTimeout(function(){
+      window.location.href = "page" + pNext + ".html";
+    }, delay);
+  }
+  else{
+    if(password != ""){
+      audio_wrong.play();
+      document.getElementById("psw").value = '';
+    }
+  }
+}
+
+var audio_cassette = document.getElementById("a_cassette");
+audio_cassette.volume = 0.5;
+
+var audio_button = document.getElementById("a_button");
+var audio_eject = document.getElementById("a_eject");
+
+function cassette(operation){
+
+  if(operation == 'play'){
+    audio_button.play();
+    if(audio_cassette.paused){
+      audio_cassette.play();
     }
     else{
-      if(password != ""){
-        audio_wrong.play();
-        document.getElementById("psw").value = '';
-      }
+      audio_cassette.pause();
     }
   }
+  else if(operation == 'rew'){
+    audio_button.play();
 
-  
-})();
+    if(audio_cassette.currentTime < 10){
+      audio_cassette.currentTime = 0;
+    }
+    else{
+      audio_cassette.currentTime -= 10;
+    }
+  }
+  else if(operation == 'ff'){
+    audio_button.play();
+    
+    if(audio_cassette.currentTime > audio_cassette.duration - 10){
+      audio_cassette.currentTime = audio_cassette.duration;
+    }
+    else{
+      audio_cassette.currentTime += 10;
+    }
+
+  }
+  else if(operation == 'stop'){
+    audio_eject.play();
+    audio_cassette.load();
+  }
+}
